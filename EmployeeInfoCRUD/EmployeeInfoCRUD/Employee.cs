47a8d822f5a6ce492952d8d634c1daac96c5a760
sgollen <38255138+sgollen@users.noreply.github.com>
2019-02-11 00:17:28 -0500

    Revert "added"
    This reverts commit 4010bba413b4f64e5ea9d6d0aa19eecfd337f6c7.

---
+++ EmployeeInfoCRUD/EmployeeInfoCRUD/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Date: Nov 20/18
+namespace PaySorter
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int HourlyPayRate { get; set; }
+
+        // Default constructor
+        public Employee()
+        {
+        }
+
+        // Constructor with inputs
+        public Employee(int EmployeeID, string Name, string Position, int PayRate)
+        {
+            this.EmployeeID= EmployeeID;
+            this.Name = Name;
+            this.Position = Position;
+            this.HourlyPayRate = PayRate;
+        }
+        // ToString override
+        public override string ToString() => $"{Name}:  ${HourlyPayRate}/hour";
+    }
+}

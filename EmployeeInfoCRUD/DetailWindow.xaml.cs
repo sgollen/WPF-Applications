d6e23c51ac5823b7b0fa5be59eaa587982a09b55
sgollen <38255138+sgollen@users.noreply.github.com>
2019-02-11 00:18:24 -0500

    Added WPF Project

---
+++ EmployeeInfoCRUD/DetailWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+// Group 2 Assignment 10 Nov 20/18
+namespace EmployeeInfo
+{
+    /// <summary>
+    /// Interaction logic for DetailWindow.xaml
+    /// </summary>
+    public partial class DetailWindow : Window
+    {
+        VM vm = null;
+        public DetailWindow(VM vm)
+        {
+            this.vm = vm;
+            InitializeComponent();
+            DataContext = vm;
+        }
+    }
+}
---
+++ EmployeeInfoCRUD/VM.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+// Date: Nov 20/18
+namespace EmployeeInfo 
+{
+    public class VM : INotifyPropertyChanged
+    {
+        //get a reference to the database object that we can use repeatedly
+        DB database = DB.GetInstance();
+
+        #region Properties
+        private int newEmployeeID = 0;
+        public int NewEmployeeID
+        {
+            get { return newEmployeeID; }
+            set { newEmployeeID = value; NotifyChanged(); }
+        }
+        private string newName = "";
+        public string NewName
+        {
+            get { return newName; }
+            set { newName = value; NotifyChanged(); }
+        }
+        private string newPosition = "";
+        public string NewPosition
+        {
+            get { return newPosition; }
+            set { newPosition = value; NotifyChanged(); }
+        }
+        private int newHourlyPayRate = 0;
+        public int NewHourlyPayRate
+        {
+            get { return newHourlyPayRate; }
+            set { newHourlyPayRate = value; NotifyChanged(); }
+        }
+
+        private BindingList<Employee> employees = new BindingList<Employee>() { };
+        public BindingList<Employee> Employees
+        {
+            get { return employees; }
+            set { employees = value; NotifyChanged(); }
+        }
+        private Employee selectedEmployee = null;
+        public Employee SelectedEmployee
+        {
+            get { return selectedEmployee; }
+            set { selectedEmployee = value; NotifyChanged(); }
+        }
+        #endregion
+
+        #region Logic
+        public VM()
+        {
+            //get a list of persons from the database
+            Employees = new BindingList<Employee>(database.Get(2));
+        }
+
+        // method to sort ascending
+        public void SortAscending()
+        {
+            Employees = new BindingList<Employee>(database.Get(1));
+        }
+
+        // method to sort descending
+        public void SortDescending()
+        {
+            Employees = new BindingList<Employee>(database.Get(0));
+        }
+
+        // method to Add new Employees
+        public void AddEmployee()
+        {
+            Employee newEmployee = new Employee(NewEmployeeID, NewName, NewPosition, NewHourlyPayRate);
+            database.Add(newEmployee);
+            Employees = new BindingList<Employee>(database.Get(2));
+        }
+
+        // method to save employees
+        public void SaveEmployees()
+        {
+            string fileText = "";
+            for (int j = 0; j < Employees.Count; j++)
+            {
+                fileText += Employees[j];
+                fileText += Environment.NewLine;
+            }
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            string appPath = Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData);
+            string dataPath = Path.Combine(appPath, "Prog8010");
+            string filePath = Path.Combine(dataPath, $"Employee.txt");
+            if (!Directory.Exists(dataPath))
+            {
+                Directory.CreateDirectory(dataPath);
+            }
+            File.WriteAllText(filePath, fileText);
+        }
+
+        // method to clears the add new employee form
+        public void ClearForm()
+        {
+            NewEmployeeID = 0;
+            NewName = "";
+            NewPosition = "";
+            NewHourlyPayRate = 0;
+        }
+        #endregion
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
